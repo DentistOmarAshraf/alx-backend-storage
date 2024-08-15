83d081615f69bc106dec1ff961c8162167d6cf0e
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-15 07:37:54 +0300

    6-bonus.sql

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus(
+    IN user_id int,
+    IN project_name varchar(255),
+    IN score int
+)
+BEGIN
+    DECLARE projectID INT;
+
+    SELECT id INTO projectID FROM projects WHERE name=project_name;
+    IF projectID IS NULL THEN
+        INSERT INTO projects(name) VALUES(project_name);
+        SET projectID = LAST_INSERT_ID();
+    END IF;
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES(user_id, projectID, score);
+END $$
