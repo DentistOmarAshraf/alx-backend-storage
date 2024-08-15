e3b5239006bf3c30832cff5b2602063db1c29de3
DentistOmarAshraf <omar_ashraf@live.com>
2024-08-15 09:27:59 +0300

    10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,18 @@
+-- Function return result of divide of two number
+-- it reutrn 0 if number / 0
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+DETERMINISTIC
+RETURNS float
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF
+    RETURN a / b;
+END $$
+
+DELIMITER ;
