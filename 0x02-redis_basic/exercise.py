b124a664cc1f913ba04c1553777dfa94af370d2b
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-11 17:52:13 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,6 +6,7 @@ import redis
 import uuid
 from typing import Union
 
+
 class Cache:
     """
     Cache - redis exercise
@@ -25,3 +26,28 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get_str(self, data: bytes) -> str:
+        """
+        get_str - decode utf8 bytes
+        """
+        if not data:
+            return None
+        return data.decode('utf8')
+
+    def get_int(self, data: bytes) -> int:
+        """
+        get_int - return int from bytes
+        """
+        if not data:
+            return None
+        return int(self.get_str(data))
+
+    def get(self, key: str, fn: callable = get_str) -> str:
+        """
+        get - get data by key
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
