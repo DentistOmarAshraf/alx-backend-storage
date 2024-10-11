54e70fdd4f0f39ef3119ab39095ba4e619507edb
DentistOmarAshraf <omar_ashraf@live.com>
2024-10-12 02:57:10 +0300

    web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Web.py
+"""
+import requests
+import redis
+from functools import wraps
+from typing import Callable
+
+clinet = redis.Redis()
+
+
+def count_url(method: Callable) -> Callable:
+    """
+    count_url
+    """
+    @wraps(method)
+    def wrapper(url):
+        if clinet.ttl(url) <= -1:
+            clinet.setex(f"count{url}", 10, 1)
+        else:
+            clinet.incr(f"count{url}")
+        return method(url)
+    return wrapper
+
+
+@count_url
+def get_page(url: str) -> str:
+    """
+    get_page
+    """
+    return requests(url).text
